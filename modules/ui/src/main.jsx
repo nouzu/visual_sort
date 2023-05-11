770f3113b86745caab5f38d9451897ab05a20f22
Dmytro Prokhorov <dfgdfg@177013.uk>
2023-05-11 14:55:18 -0400

    Add prettier

--- modules/ui/src/main.jsx
+++ modules/ui/src/main.jsx
@@ -1,97 +1,103 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-import "./global.css"
-
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListSubheader from "@mui/material/ListSubheader";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import Slider from "@mui/material/Slider";
-import Typography from "@mui/material/Typography";
-
-import ShuffleIcon from '@mui/icons-material/Shuffle';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import BubbleChartIcon from '@mui/icons-material/BubbleChart';
+import CallMergeIcon from '@mui/icons-material/CallMerge';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import LocalBarIcon from '@mui/icons-material/LocalBar';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import SpaceBarIcon from '@mui/icons-material/SpaceBar';
 import SpeedIcon from '@mui/icons-material/Speed';
 import SwitchAccessShortcutAddIcon from '@mui/icons-material/SwitchAccessShortcutAdd';
-import CallMergeIcon from '@mui/icons-material/CallMerge';
-import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
-import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
-import SpaceBarIcon from '@mui/icons-material/SpaceBar';
-import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
-import LocalBarIcon from '@mui/icons-material/LocalBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import init, {
+  shuffle,
+  reverse,
+  bubble_sort,
+  insertion_sort,
+  merge_sort,
+  quick_sort,
+  shell_sort,
+  selection_sort,
+  cocktail_sort,
+} from 'renderer';
 
-import init, {shuffle, reverse, bubble_sort, insertion_sort, merge_sort, quick_sort, shell_sort, selection_sort, cocktail_sort} from "renderer"
+import './global.css';
 
 const App = () => {
   const actions = [
     {
-      title: "Shuffle",
-      icon: <ShuffleIcon/>,
-      onClick: () => shuffle()
+      title: 'Shuffle',
+      icon: <ShuffleIcon />,
+      onClick: () => shuffle(),
     },
     {
-      title: "Reverse",
-      icon: <SettingsBackupRestoreIcon/>,
-      onClick: () => reverse()
-    }
+      title: 'Reverse',
+      icon: <SettingsBackupRestoreIcon />,
+      onClick: () => reverse(),
+    },
   ];
 
   const algorithms = [
     {
-      title: "Bubble Sort",
-      icon: <BubbleChartIcon/>,
+      title: 'Bubble Sort',
+      icon: <BubbleChartIcon />,
       onClick: () => runAlgorithm(bubble_sort),
     },
     {
-      title: "Cocktail Sort",
-      icon: <LocalBarIcon/>,
-      onClick: () => runAlgorithm(cocktail_sort)
+      title: 'Cocktail Sort',
+      icon: <LocalBarIcon />,
+      onClick: () => runAlgorithm(cocktail_sort),
     },
     {
-      title: "Insertion Sort",
-      icon: <SwitchAccessShortcutAddIcon/>,
-      onClick: () => runAlgorithm(insertion_sort)
+      title: 'Insertion Sort',
+      icon: <SwitchAccessShortcutAddIcon />,
+      onClick: () => runAlgorithm(insertion_sort),
     },
     {
-      title: "Merge Sort",
-      icon: <CallMergeIcon/>,
-      onClick: () => runAlgorithm(merge_sort)
+      title: 'Merge Sort',
+      icon: <CallMergeIcon />,
+      onClick: () => runAlgorithm(merge_sort),
     },
     {
-      title: "Quick Sort",
-      icon: <ElectricBoltIcon/>,
-      onClick: () => runAlgorithm(quick_sort)
+      title: 'Quick Sort',
+      icon: <ElectricBoltIcon />,
+      onClick: () => runAlgorithm(quick_sort),
     },
     {
-      title: "Shell Sort",
-      icon: <SpaceBarIcon/>,
-      onClick: () => runAlgorithm(shell_sort)
+      title: 'Shell Sort',
+      icon: <SpaceBarIcon />,
+      onClick: () => runAlgorithm(shell_sort),
     },
     {
-      title: "Selection Sort",
-      icon: <RadioButtonCheckedIcon/>,
-      onClick: () => runAlgorithm(selection_sort)
-    }
-  ]
+      title: 'Selection Sort',
+      icon: <RadioButtonCheckedIcon />,
+      onClick: () => runAlgorithm(selection_sort),
+    },
+  ];
 
   const runAlgorithm = (func) => {
     setRunning(true);
 
-    func(speed)
-      .then(() => setRunning(false));
-  }
+    func(speed).then(() => setRunning(false));
+  };
 
   const [initializing, setInitializing] = React.useState(true);
   const [running, setRunning] = React.useState(false);
@@ -101,56 +107,48 @@ const App = () => {
     if (initializing) {
       init().then(() => setInitializing(false));
     }
-  })
+  });
 
   const disabled = running || initializing;
 
   return (
     <React.StrictMode>
-      <CssBaseline/>
-      <Box sx={{display: "flex"}}>
-        <Box component="main" id="m" sx={{width: 800, height: 600}}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex' }}>
+        <Box component="main" id="m" sx={{ width: 800, height: 600 }}>
           {/* the canvas will be here */}
         </Box>
-        <Box component="aside" sx={{width: 300, height: 600}}>
+        <Box component="aside" sx={{ width: 300, height: 600 }}>
           <List subheader={<ListSubheader>Actions</ListSubheader>}>
             {actions.map((action) => (
-              <ListItem key={action.title} sx={{p: 0}}>
+              <ListItem key={action.title} sx={{ p: 0 }}>
                 <ListItemButton disabled={disabled} onClick={action.onClick}>
-                  <ListItemIcon>
-                    {action.icon}
-                  </ListItemIcon>
-                  <ListItemText>
-                    {action.title}
-                  </ListItemText>
+                  <ListItemIcon>{action.icon}</ListItemIcon>
+                  <ListItemText>{action.title}</ListItemText>
                 </ListItemButton>
               </ListItem>
             ))}
           </List>
-          <Divider/>
+          <Divider />
           <List>
             {algorithms.map((algorithm) => (
-              <ListItem key={algorithm.title} sx={{p: 0}}>
+              <ListItem key={algorithm.title} sx={{ p: 0 }}>
                 <ListItemButton disabled={disabled} onClick={algorithm.onClick}>
-                  <ListItemIcon>
-                    {algorithm.icon}
-                  </ListItemIcon>
-                  <ListItemText>
-                    {algorithm.title}
-                  </ListItemText>
+                  <ListItemIcon>{algorithm.icon}</ListItemIcon>
+                  <ListItemText>{algorithm.title}</ListItemText>
                 </ListItemButton>
               </ListItem>
             ))}
           </List>
-          <Divider/>
-          <Stack spacing={2} sx={{m: 2,textAlign:"center"}}>
+          <Divider />
+          <Stack spacing={2} sx={{ m: 2, textAlign: 'center' }}>
             <Stack
               spacing={2}
               direction="row"
               alignItems="center"
-              sx={{px: 2}}
+              sx={{ px: 2 }}
             >
-              <SpeedIcon sx={{mr: 1}}/>
+              <SpeedIcon sx={{ mr: 1 }} />
               <Slider
                 disabled={disabled}
                 min={1}
@@ -168,10 +166,9 @@ const App = () => {
         </Box>
       </Box>
     </React.StrictMode>
-  )
-}
+  );
+};
 
-const root = document.getElementById("root")
+const root = document.getElementById('root');
 
-ReactDOM.createRoot(root)
-  .render(<App/>);
+ReactDOM.createRoot(root).render(<App />);
--- modules/ui/vite.config.js
+++ modules/ui/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import wasmPack from "vite-plugin-wasm-pack";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), wasmPack("../renderer")],
+  plugins: [react(), wasmPack('../renderer')],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
@@ -16,12 +16,12 @@ export default defineConfig(async () => ({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
